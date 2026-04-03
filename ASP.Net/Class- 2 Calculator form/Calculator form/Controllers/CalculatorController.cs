61afeb10885649c3d00aaa646e6e974f778432e2
Students <me-Aimantariq>
2026-04-03 15:43:26 +0500

    Class-02 Calculator form

---
+++ ASP.Net/Class- 2 Calculator form/Calculator form/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Calculator_form.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int num1, int num2, string operation)
+        {
+            int result = 0;
+
+            switch (operation)
+            {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Subtract":
+                    result = num1 - num2;
+                    break;
+                case "Multiply":
+                    result = num1 * num2;
+                    break;
+                case "Divide":
+                    result = num1 / num2;
+                    break;
+            }
+            ViewBag.data = result;
+            return View();
+        }
+    }
+}
